8ba64371d30bf6de27aaa7b842857873ddb64f2d
Phong Bui <boushphong@gmail.com>
2023-04-16 03:28:58 +0700

    Version 2: Add RDS deployment

--- main.tf
+++ main.tf
@@ -1,42 +1,59 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.63.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region     = "ap-southeast-1"
   access_key = ""
   secret_key = ""
 }
 
-# Create a new VPC
-resource "aws_vpc" "bastion_vpc" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "default" {
+  default = true
+}
 
-  tags = {
-    Name = "bastion-vpc"
-  }
+# Default subnets
+data "aws_subnet" "subnet_1a" {
+  vpc_id            = data.aws_vpc.default.id
+  availability_zone = "ap-southeast-1a"
+  default_for_az    = true
 }
 
 # Create a new subnet in the VPC
-resource "aws_subnet" "bastion_subnet" {
-  vpc_id     = aws_vpc.bastion_vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "bastion_subnet_a" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.48.0/20"
   availability_zone = "ap-southeast-1a"
-
-  tags = {
-    Name = "bastion-subnet"
-  }
 }
 
-# Create a security group for the bastion host that allows incoming traffic on port 22 from anywhere
+resource "aws_subnet" "bastion_subnet_b" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.64.0/20"
+  availability_zone = "ap-southeast-1b"
+}
+
+resource "aws_route_table" "bastion_rt" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_route_table_association" "subnet_1_association" {
+  subnet_id      = aws_subnet.bastion_subnet_a.id
+  route_table_id = aws_route_table.bastion_rt.id
+}
+
+resource "aws_route_table_association" "subnet_2_association" {
+  subnet_id      = aws_subnet.bastion_subnet_b.id
+  route_table_id = aws_route_table.bastion_rt.id
+}
+
 resource "aws_security_group" "bastion_sg" {
   name_prefix = "bastion-sg-"
-  vpc_id      = aws_vpc.bastion_vpc.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -46,52 +63,60 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-# Create an Internet Gateway for the VPC
-resource "aws_internet_gateway" "bastion_igw" {
-  vpc_id = aws_vpc.bastion_vpc.id
-
-  tags = {
-    Name = "bastion-igw"
-  }
-}
-
-# Create a Route Table for the VPC
-resource "aws_route_table" "bastion_rt" {
-  vpc_id = aws_vpc.bastion_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.bastion_igw.id
-  }
-
-  tags = {
-    Name = "bastion-rt"
-  }
-}
-
-# Associate the Route Table with the subnet
-resource "aws_route_table_association" "bastion_rta" {
-  subnet_id      = aws_subnet.bastion_subnet.id
-  route_table_id = aws_route_table.bastion_rt.id
-}
-
 # Create an EC2 instance as a bastion host in the new VPC
 resource "aws_instance" "bastion_host" {
-  ami           = "ami-0a72af05d27b49ccb"
-  instance_type = "t2.micro"
-  key_name      = "my_key_pair"
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  subnet_id     = aws_subnet.bastion_subnet.id
+  ami                         = "ami-0a72af05d27b49ccb"
+  instance_type               = "t2.micro"
+  key_name                    = "my_key_pair"
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  subnet_id                   = data.aws_subnet.subnet_1a.id
   associate_public_ip_address = true
 }
 
-# Allocate an Elastic IP address for the bastion host
-resource "aws_eip" "bastion_eip" {
-  vpc = true
+resource "aws_db_subnet_group" "db_subnet_group_1" {
+  name        = "db-subnet-group"
+  description = "DB subnet group with subnet in ap-southeast-1a and ap-southeast-1b"
+
+  subnet_ids = [
+    aws_subnet.bastion_subnet_a.id,
+    aws_subnet.bastion_subnet_b.id,
+  ]
 }
 
-# Associate the Elastic IP address with the bastion host
-resource "aws_eip_association" "bastion_eip_assoc" {
-  instance_id   = aws_instance.bastion_host.id
-  allocation_id = aws_eip.bastion_eip.id
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds_sg"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+}
+
+resource "aws_db_instance" "my_db_instance" {
+  identifier             = "my-db-instance"
+  engine                 = "postgres"
+  engine_version         = "14.6"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group_1.name
+  publicly_accessible    = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  username               = "postgres"
+  password               = "postgres"
+  db_name                = "postgres"
+  port                   = 5432
+  skip_final_snapshot    = true
+  multi_az               = false
+}
+
+output "aws_instance_public_ip" {
+  value = aws_instance.bastion_host.public_ip
+}
+
+output "aws_rds" {
+  value = aws_db_instance.my_db_instance.address
 }
