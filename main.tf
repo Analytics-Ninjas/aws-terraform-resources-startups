591474dd4d31c8acd52f9ea15f876b64921e018f
Bui Duc Phong[ Bui Duc Phong ] <bui.duc.phong@linecorp.com>
2023-04-20 17:36:07 +0700

    Change region to JP, add s3, key_pair resource

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region     = "ap-southeast-1"
+  region     = "ap-northeast-1"
   access_key = ""
   secret_key = ""
 }
 
+# VPC
 data "aws_vpc" "default" {
   default = true
 }
@@ -20,7 +21,7 @@ data "aws_vpc" "default" {
 # Default subnets
 data "aws_subnet" "subnet_1a" {
   vpc_id            = data.aws_vpc.default.id
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-northeast-1a"
   default_for_az    = true
 }
 
@@ -28,15 +29,16 @@ data "aws_subnet" "subnet_1a" {
 resource "aws_subnet" "bastion_subnet_a" {
   vpc_id            = data.aws_vpc.default.id
   cidr_block        = "172.31.48.0/20"
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-northeast-1a"
 }
 
-resource "aws_subnet" "bastion_subnet_b" {
+resource "aws_subnet" "bastion_subnet_d" {
   vpc_id            = data.aws_vpc.default.id
   cidr_block        = "172.31.64.0/20"
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "ap-northeast-1d"
 }
 
+# Route Table
 resource "aws_route_table" "bastion_rt" {
   vpc_id = data.aws_vpc.default.id
 }
@@ -47,10 +49,11 @@ resource "aws_route_table_association" "subnet_1_association" {
 }
 
 resource "aws_route_table_association" "subnet_2_association" {
-  subnet_id      = aws_subnet.bastion_subnet_b.id
+  subnet_id      = aws_subnet.bastion_subnet_d.id
   route_table_id = aws_route_table.bastion_rt.id
 }
 
+# Security Group
 resource "aws_security_group" "bastion_sg" {
   name_prefix = "bastion-sg-"
   vpc_id      = data.aws_vpc.default.id
@@ -71,23 +74,39 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
+# Keys
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = "my_key_pair"       # Create "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./my_key_pair.pem"
+  }
+}
+
 # Create an EC2 instance as a bastion host in the new VPC
 resource "aws_instance" "bastion_host" {
-  ami                         = "ami-0a72af05d27b49ccb"
+  ami                         = "ami-0d979355d03fa2522"
   instance_type               = "t2.micro"
-  key_name                    = "my_key_pair"
+  key_name                    = aws_key_pair.kp.key_name
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   subnet_id                   = data.aws_subnet.subnet_1a.id
   associate_public_ip_address = true
 }
 
+# AWS RDS
 resource "aws_db_subnet_group" "db_subnet_group_1" {
   name        = "db-subnet-group"
-  description = "DB subnet group with subnet in ap-southeast-1a and ap-southeast-1b"
+  description = "DB subnet group with subnet in ap-southeast-1a and ap-southeast-1d"
 
   subnet_ids = [
     aws_subnet.bastion_subnet_a.id,
-    aws_subnet.bastion_subnet_b.id,
+    aws_subnet.bastion_subnet_d.id,
   ]
 }
 
@@ -117,14 +136,30 @@ resource "aws_db_instance" "my_db_instance" {
   password               = "postgres"
   db_name                = "postgres"
   port                   = 5432
-  skip_final_snapshot    = true
+  skip_final_snapshot     = true
   multi_az               = false
 }
 
+# S3
+resource "random_string" "bucket_suffix" {
+  length  = 6
+  special = false
+  upper = false
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "analytics-ninjas-${random_string.bucket_suffix.result}"
+}
+
+# Output
 output "aws_instance_public_ip" {
   value = aws_instance.bastion_host.public_ip
 }
 
 output "aws_rds" {
-  value = aws_db_instance.my_db_instance.address
+  value = aws_db_instance.my_db_instance.address  
+}
+
+output "aws_s3_bucket_name" {
+  value = aws_s3_bucket.s3_bucket.bucket
 }
